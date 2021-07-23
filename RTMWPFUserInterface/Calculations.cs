b25cc1980b9585e373a31c958c69e89598ce54ed
Raheel <kamal_raheel@hotmail.com>
2021-07-23 14:40:12 -0400

    Check DI whether its working correctly

---
+++ RTMWPFUserInterface/Calculations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTMWPFUserInterface
+{
+    public class Calculations : ICalculations
+    {
+        public List<string> Register { get; set; } = new List<string>();
+
+        public double addDouble(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
