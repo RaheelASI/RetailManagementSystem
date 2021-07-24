d61083d25aea76d2c7ec49407363bb3166220621
Raheel <kamal_raheel@hotmail.com>
2021-07-24 13:57:03 -0400

    Test branching

---
+++ RTMWPFUserInterface/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTMWPFUserInterface
+{
+    public class TestClass
+    {
+        public List<string> strList { get; set; } = new List<string>();
+    }
+}
